9302384ec5c8a1eeb5a3d014ca8f0c77a2247ed1
shengwang52005 <shengwang52005@163.com>
2024-01-24 17:50:21 +0800

    test3

--- 一键提交.bat
+++ 一键提交.bat
@@ -1,12 +1,16 @@
 @echo off
 git add .
 set /p commit=Commit message...
-echo .
-git commit -m "%commit%"
 echo Commit message:%commit%
+echo ====================
+git commit -m "%commit%"
+echo ====================
 echo Are you sure?
 pause
+echo ====================
 git push origin main
+echo ====================
 echo Done!
 pause
-echo
+echo ====================
+exit
